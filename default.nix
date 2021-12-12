7b1abdbb99a1ff5c7e2db8eb8e9d7e114c804a37
Anselm Schüler <mail@anselmschueler.com>
2021-12-12 20:32:06 +0100

    fix init

--- default.nix
+++ default.nix
@@ -2,6 +2,6 @@ let
   lib = builtins.foldl' (a1: a2: a1 // a2) { } (builtins.attrValues
     (builtins.mapAttrs (k: v: {
       ${builtins.elemAt (builtins.match "(.*)\\.nix" k) 0} =
-        import (./. + "/${k}") { inherit lib; };
+        import (./moules + "/${k}") { inherit lib; };
     }) (builtins.readDir ./modules)));
 in lib
