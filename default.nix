57ee2cff870197e53dd0cce988490aeb9c1650b6
Anselm Schüler <mail@anselmschueler.com>
2021-12-12 20:31:41 +0100

    new folder structure and fixing bootstrap

--- default.nix
+++ default.nix
@@ -1,9 +1,7 @@
 let
   lib = builtins.foldl' (a1: a2: a1 // a2) { } (builtins.attrValues
-    (builtins.mapAttrs (k: v:
-      if k != "default.nix" && v == "regular" then {
-        ${builtins.elemAt (builtins.match "(.*)\\.nix" k) 0} =
-          import (./. + "/${k}") { inherit lib; };
-      } else
-        { }) (builtins.readDir ./.)));
+    (builtins.mapAttrs (k: v: {
+      ${builtins.elemAt (builtins.match "(.*)\\.nix" k) 0} =
+        import (./. + "/${k}") { inherit lib; };
+    }) (builtins.readDir ./modules)));
 in lib
