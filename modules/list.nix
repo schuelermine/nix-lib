e130d6fb0f8cdc8545d223e874d73146f5ab5db5
Anselm Schüler <mail@anselmschueler.com>
2022-01-13 19:55:47 +0100

    rename to singletonIfNotList

--- modules/list.nix
+++ modules/list.nix
@@ -1,5 +1,5 @@
 { self }: rec {
   flatMap = f: xs: builtins.concatLists (map f xs);
-  wrapIfNotList = x: if !builtins.isList x then [ x ] else x;
+  singletonIfNotList = x: if !builtins.isList x then [ x ] else x;
   singleton = x: [ x ];
 }
