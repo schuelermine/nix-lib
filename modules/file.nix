62d32d303cc0e6fbca120d25de47a82090a25240
Anselm Schüler <mail@anselmschueler.com>
2022-01-14 20:06:37 +0100

    Add readDirRecursiveWith

--- modules/file.nix
+++ modules/file.nix
@@ -1,12 +1,13 @@
 self:
 with self; rec {
-  getFilesRecursive = dir:
+  readDirRecursive = readDirRecursiveWith (dir: name: "${dir}/${name}");
+  readDirRecursiveWith = f: dir:
     builtins.foldl' (s1: s2: s1 // s2) { } (builtins.concatLists
       (builtins.attrValues (builtins.mapAttrs (name: type:
         if type == "directory" then
           builtins.attrValues
           (builtins.mapAttrs (name2: type2: { ${name + "/" + name2} = type2; })
-            (getFilesRecursive (dir + "/${name}")))
+            (readDirRecursiveWith (f dir name)))
         else [{
           ${name} = type;
         }]) (builtins.readDir dir))));
