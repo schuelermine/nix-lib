aea472094b557353ab2da1275d1bd571bde19214
Anselm Schüler <mail@anselmschueler.com>
2022-01-15 09:27:04 +0100

    Change & de-duplicate bootstrap

--- default.nix
+++ default.nix
@@ -1,7 +1,13 @@
 let
+  dir = ./modules;
   self = builtins.foldl' (a1: a2: a1 // a2) { } (builtins.attrValues
     (builtins.mapAttrs (k: _: {
-      ${builtins.head (builtins.match "(.*)\\.nix" k)} =
-        import (./modules + "/${k}") self;
-    }) (builtins.readDir ./modules)));
+      ${
+        let matches = (builtins.match "(.*)\\.nix" k);
+        in if matches != null && builtins.length matches == 1 then
+          builtins.head matches
+        else
+          null
+      } = import (dir + "/${k}") self;
+    }) (builtins.readDir dir)));
 in self
--- flake.nix
+++ flake.nix
@@ -1,8 +1,1 @@
-{
-  outputs = { self }:
-    builtins.foldl' (a1: a2: a1 // a2) { } (builtins.attrValues
-      (builtins.mapAttrs (k: _: {
-        ${builtins.head (builtins.match "(.*)\\.nix" k)} =
-          import (./modules + "/${k}") self.outputs;
-      }) (builtins.readDir ./modules)));
-}
+{ outputs = { self }: import ./default.nix; }
