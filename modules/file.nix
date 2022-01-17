34f241a32bf3c08821aa2dcd6279298142a696f5
Anselm Schüler <mail@anselmschueler.com>
2022-01-17 19:43:23 +0100

    revamp file.nix

--- modules/file.nix
+++ modules/file.nix
@@ -1,16 +1,20 @@
 self:
 with self; rec {
-  readDirRecursive = readDirRecursiveSep "/";
-  readDirRecursiveSep = sep:
-    readDirRecursiveWith (dir: name: "${dir}${sep}${name}");
-  readDirRecursiveWith = f: dir:
+  readDirRCollapsed = readDirRCollapsedSep "/";
+  readDirRCollapsedSep = sep:
+    readDirRCollapsedWith (dir: name: "${dir}${sep}${name}");
+  readDirRCollapsedWith = f: dir:
     builtins.foldl' (s1: s2: s1 // s2) { } (builtins.concatLists
       (builtins.attrValues (builtins.mapAttrs (name: type:
         if type == "directory" then
           builtins.attrValues
           (builtins.mapAttrs (name2: type2: { ${f name name2} = type2; })
-            (readDirRecursiveWith f (dir + "/${name}")))
+            (readDirRCollapsedWith f (dir + "/${name}")))
         else [{
           ${name} = type;
         }]) (builtins.readDir dir))));
+  readDirR = dir:
+    builtins.mapAttrs (name: type:
+      if type == "directory" then readDirR (dir + "/${name}") else type)
+    (builtins.readDir dir);
 }
