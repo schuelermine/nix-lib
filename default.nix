dc8a383ab330b120e2cfe5877ae3fa195f3046b4
Anselm Schüler <mail@anselmschueler.com>
2022-05-30 21:27:27 +0200

    S

--- default.nix
+++ default.nix
@@ -1,21 +1,21 @@
 { nixpkgs ? throw "Nixpkgs not provided." }:
 let
-  load-module = deps@{ nixpkgs }:
+  load-module = args@{ lib, nixpkgs }:
     final: module:
     if builtins.isFunction module then
-      load-module deps final (module final) null
+      load-module args (module args) null
     else if builtins.isString module then
-      load-module deps final (./modules + "/${module}")
+      load-module args final (./modules + "/${module}")
     else if builtins.isPath module then
-      load-module deps final (import module)
+      load-module args final (import module)
     else if builtins.isAttrs module then
       module
     else
       throw "Loading module failed at value ${module}";
   fix-modules = modules:
     let
-      final = builtins.foldl' (a: b: a // b) { }
-        (builtins.map (load-module { inherit nixpkgs; } final) modules);
-    in final;
+      lib = builtins.foldl' (a: b: a // b) { }
+        (builtins.map (load-module { inherit nixpkgs lib; }) modules);
+    in lib;
   modules = builtins.attrNames (builtins.readDir ./modules);
 in fix-modules modules
