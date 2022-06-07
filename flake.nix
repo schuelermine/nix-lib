5394bb5a38d5e3144554a3bf2ac72adf60975ccf
Anselm Schüler <mail@anselmschueler.com>
2022-06-07 13:20:47 +0200

    preCall

--- flake.nix
+++ flake.nix
@@ -1,1 +1,20 @@
-{ outputs = { self }: { lib = import ./default.nix; }; }
+{
+  outputs = { self }:
+    let
+      preCall = with builtins;
+        f:
+        let
+          g = a:
+            if isAttrs a then
+              let x = f a;
+              in {
+                __functor = self: b: g b;
+              } // x // {
+                ${if x ? __functor then null else "__functionArgs"} =
+                  functionArgs f;
+              }
+            else
+              f a;
+        in g { };
+    in { lib = preCall (import ./default.nix); };
+}
