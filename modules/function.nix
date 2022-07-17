11978375869b25a37decdc156d1283d4059d5083
Anselm Schüler <mail@anselmschueler.com>
2022-07-17 21:13:33 +0200

    Add function.nix stuff

--- modules/function.nix
+++ modules/function.nix
@@ -1,1 +1,23 @@
-{ lib, ... }: with builtins; with lib; { id = x: x; }
+{ lib, ... }:
+with builtins;
+with lib; {
+  id = x: x;
+  const = x: _: x;
+  compose = f: g: x: f (g x);
+  flip = f: x: y: f y x;
+  fix = f: let x = f x; in x;
+  preCall = f: i:
+    let
+      g = a:
+        if isAttrs a then
+          let x = f a;
+          in {
+            __functor = self: b: g b;
+          } // x // {
+            ${if x ? __functor then null else "__functionArgs"} =
+              functionArgs f;
+          }
+        else
+          f a;
+    in g i;
+}
